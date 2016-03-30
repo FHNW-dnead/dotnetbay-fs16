632dda80eb4da79f0e4ec0544a68abfa647a9a29
Martin Kropp <martin.kropp@fhnw.ch>
2016-03-30 22:28:46 +0200

    added Location field to Member entity

--- source/DotNetBay.Data.EF/Migrations/Configuration.cs
+++ source/DotNetBay.Data.EF/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace DotNetBay.Data.EF.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(DotNetBay.Data.EF.MainDbContext context)
