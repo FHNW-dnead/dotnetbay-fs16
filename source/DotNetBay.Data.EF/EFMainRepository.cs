e0152695d5d3e7ea7f4c4807e92b21e687f6c40a
Martin Kropp <martin.kropp@fhnw.ch>
2016-03-30 22:01:51 +0200

    added sample solution til task 5 (before migrations)

---
+++ source/DotNetBay.Data.EF/EFMainRepository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotNetBay.Interfaces;
+using DotNetBay.Model;
+
+namespace DotNetBay.Data.EF
+{
+    public class EFMainRepository : IMainRepository
+    {
+        private MainDbContext context;
+
+        public EFMainRepository()
+        {
+            this.context = new MainDbContext();
+
+        }
+
+        public Database Database
+        {
+            get
+            {
+                return this.context.Database;
+            }
+        }
+
+        public IQueryable<Auction> GetAuctions()
+        {
+            return this.context.Auctions.Include(a => a.Bids).Include(a => a.Seller).Include(a => a.ActiveBid).Include(a => a.Winner);
+        }
+
+        public IQueryable<Member> GetMembers()
+        {
+            return this.context.Members.Include(m => m.Auctions).Include(m => m.Bids);
+        }
+
+        public Bid GetBidByTransactionId(Guid transactionId)
+        {
+            return this.context.Bids.Include(b => b.Auction).Include(b => b.Bidder).FirstOrDefault(b => b.TransactionId == transactionId);
+        }
+
+        public void SaveChanges()
+        {
+            this.context.SaveChanges();
+        }
+
+        public Member Add(Member member)
+        {
+            this.context.Members.Add(member);
+
+            return member;
+        }
+
+        public Bid Add(Bid bid)
+        {
+            this.context.Bids.Add(bid);
+
+            return bid;
+        }
+
+        public Auction Update(Auction auction)
+        {
+            return auction;
+        }
+
+        public Auction Add(Auction auction)
+        {
+            this.context.Auctions.Add(auction);
+
+            return auction;
+        }
+    }
+}
---
+++ source/DotNetBay.Test/Storage/EFMainRepositoryTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotNetBay.Data.EF;
+using DotNetBay.Interfaces;
+
+namespace DotNetBay.Test.Storage
+{
+    class EFMainRepositoryTests : MainRepositoryTestBase
+    {
+        protected override IRepositoryFactory CreateFactory()
+        {
+            return new EFMainRepositoryFactory();
+        }
+    }
+
+    public class EFMainRepositoryFactory : IRepositoryFactory
+    {
+        private List<EFMainRepository> repos = new List<EFMainRepository>();
+
+        public void Dispose()
+        {
+            foreach (var repo in this.repos)
+            {
+                repo.Database.Delete();
+            }
+        }
+
+        public IMainRepository CreateMainRepository()
+        {
+            var repo = new EFMainRepository();
+
+            if (!this.repos.Any())
+            {
+                repo.Database.Delete();
+            }
+
+            this.repos.Add(repo);
+
+            return repo;
+        }
+    }
+
+}
