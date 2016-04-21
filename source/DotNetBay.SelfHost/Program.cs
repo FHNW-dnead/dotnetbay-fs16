100622d8eab07ad190558bd8a2a847c90c5fb41b
Michael Schnyder <michael.schnyder@zuehlke.com>
2016-04-21 23:23:43 +0200

    WebServer with StatusController

---
+++ source/DotNetBay.SelfHost/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+
+using DotNetBay.WebApi;
+
+namespace DotNetBay.SelfHost
+{
+    /// <summary>
+    /// Main Entry for program
+    /// </summary>
+    public static class Program
+    {
+        public static void Main()
+        {
+            var type = typeof(StatusController);
+
+            Console.WriteLine("DotNetBay SelfHost");
+
+            using (WebApp.Start<Startup>(url: "http://localhost:9001/"))
+            {
+
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ source/DotNetBay.SelfHost/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(DotNetBay.SelfHost.Startup))]
+
+namespace DotNetBay.SelfHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            var httpConfiguration = new HttpConfiguration();
+            
+            httpConfiguration.MapHttpAttributeRoutes();
+
+            app.UseWebApi(httpConfiguration);
+        }
+    }
+}
