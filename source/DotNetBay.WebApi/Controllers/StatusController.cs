0f95abed3a20515c4f56cdceb02def02fd89fa0d
Michael Schnyder <michael.schnyder@zuehlke.com>
2016-04-21 23:38:39 +0200

    moved to controllers folder

---
+++ source/DotNetBay.WebApi/Controllers/StatusController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace DotNetBay.WebApi.Controller
+{
+    public class StatusController : ApiController
+    {
+        [HttpGet]
+        [Route("api/status")]
+        public IHttpActionResult AreYouFine()
+        {
+            return this.Ok("I'm fine");
+        }
+    }
+}
