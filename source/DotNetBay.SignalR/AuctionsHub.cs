1f3fdb62d21cfdca99ab8dd8218a9014501b573a
Michael Schnyder <michael.schnyder@zuehlke.com>
2016-04-25 23:58:04 +0200

    added hub and static methods to call clients from external code

---
+++ source/DotNetBay.SignalR/AuctionsHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.SignalR;
+using DotNetBay.Model;
+
+namespace DotNetBay.SignalR
+{
+    public class AuctionsHub : Hub
+    {
+        public static void NotifyNewAuction(Auction auction)
+        {
+            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.NewAuction(auction.Id);
+        }
+
+        public static void NotifyBidAccepted(Auction auction, Bid bid)
+        {
+            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.NotifyBidAccepted(auction.Id, bid.Id);
+        }
+
+        public static void NotifyAuctionStarted(Auction auction)
+        {
+            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.NotifyAuctionStarted(auction.Id);
+        }
+
+        public static void NotifyAuctionEnded(Auction auction)
+        {
+            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.NotifyAuctionEnded(auction.Id);
+        }
+
+    }
+}
