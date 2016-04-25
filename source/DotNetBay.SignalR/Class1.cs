249db9bc690aa6e1faae12d89c527ab6b9da591e
Michael Schnyder <michael.schnyder@zuehlke.com>
2016-04-25 23:33:13 +0200

    added signalR project

---
+++ source/DotNetBay.SignalR/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetBay.SignalR
+{
+    public class Class1
+    {
+    }
+}
