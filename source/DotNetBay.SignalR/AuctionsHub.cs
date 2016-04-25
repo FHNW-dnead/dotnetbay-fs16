d9425e1c146afcef26eec25d40b2c4101565b486
Michael Schnyder <michael.schnyder@zuehlke.com>
2016-04-26 00:26:40 +0200

    changed client methods a bit

--- source/DotNetBay.SignalR/AuctionsHub.cs
+++ source/DotNetBay.SignalR/AuctionsHub.cs
@@ -13,22 +13,22 @@ namespace DotNetBay.SignalR
 
         public static void NotifyNewBid(Auction auction, Bid newBid)
         {
-            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.NotifyNewBid(auction.Id);
+            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.NewBid(auction.Id);
         }
 
         public static void NotifyBidAccepted(Auction auction, Bid bid)
         {
-            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.NotifyBidAccepted(auction.Id, bid.Id);
+            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.BidAccepted(auction.Id, bid.Id);
         }
 
         public static void NotifyAuctionStarted(Auction auction)
         {
-            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.NotifyAuctionStarted(auction.Id);
+            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.AuctionStarted(auction.Id);
         }
 
         public static void NotifyAuctionEnded(Auction auction)
         {
-            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.NotifyAuctionEnded(auction.Id);
+            GlobalHost.ConnectionManager.GetHubContext<AuctionsHub>().Clients.All.AuctionEnded(auction.Id);
         }
     }
 }
